d8d1f3507a67cba00c81a28d9ea5bc4d498b892f
bpetterborg <ben.petterborg@gmail.com>
2021-12-04 14:06:56 -0500

    Finished display setup

--- pioled_api/pioled_api.py
+++ pioled_api/pioled_api.py
@@ -9,29 +9,72 @@
 
 import asyncio			# Asynchronous I/O
 import websockets		# Websockets
+import json				# reading config
+from time import sleep
 
-import busio									# i2c
-from Board import SCL, SDA						# i2c probably
+import Adafruit_GPIO.SPI as SPI	
 import adafruit_ssd1306							# piOLED
-from time import sleep							# sleep
+
 from PIL import Image, ImageDraw, ImageFont		# fonts and pictures
 
-class PiOLED:
+
+# reading config, some setup
+with open('config.json') as configFile:
+	config = json.load(configFile)
+
+resX = config['resX']
+resY = config['resY']
 	
-	def __init__(self) -> None:
+font = ImageFont.load_default() # eventally change this read from the config
+
+# gpio stuff
+rst = None	# unused pin
+dc = 23
+spiPort = 0
+spiDevice = 0
+
+# setup screen
+display = adafruit_ssd1306.SSD1306_128_32(rst=rst)
+
+display.begin()		# init and clear
+display.clear()
+display.display()
+
+# make something to draw onto
+width = display.width
+height = display.height
+
+image = Image.new('1', (width, height))					# 1 bit mode
+draw = ImageDraw.Draw(image)	
+draw.rectangle((0,0,width,height), outline=0, fill=0) 	# clear screen	
+
+# padding
+padding = -2
+top = padding
+bottom = height - padding
+
+x = 0	# start at the left
+
+
+# some functions
+	
+def __init__(self) -> None:
 		pass
 
-	async def ping(websocket):
-		await websocket.send("pong")
+async def ping(websocket):
+	await websocket.send("pong")
 
-	async def write(websocket, text, ):
-		pass
+async def write(websocket, text):
+	pass
 
-	async def draw(websocket, x, y, image):
-		pass
-
-	async def clear(websocket):
-		pass
+async def clear(websocket):
+	pass
 
 # i think this is correct??
-asyncio.run(PiOLED)
+
+
+server = websockets.serve(write, "localhost", 8765)
+listener = websockets.listen("localhost", 8765)			#  unsure if this is needed, will keep it for now.
+
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
